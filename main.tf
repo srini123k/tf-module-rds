445049d0ba521f925cffb39c2367b879a5fc0051
Srini K <srinikdevops0131@gmail.com>
2023-03-22 11:51:17 -0700

    updating ./tf-module-rds

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_rds_cluster" "main" {
+  cluster_identifier      = "${var.env}-rds"
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  database_name           = var.database_name
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  #kms_key_id              = data.aws_kms_key.key.arn
+  storage_encrypted       = var.storage_encrypted
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-rds" }
+  )
+}
+resource "aws_rds_cluster_instance" "main" {
+  count              = var.no_of_instances
+  identifier         = "${var.env}-rds-${count.index}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = var.instance_class
+  engine             = var.engine
+  engine_version     = var.engine_version
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.env}-rds"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-subnet-group" }
+  )
+}
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "database_name" {
+  default = "dummy"
+}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "no_of_instances" {}
+variable "instance_class" {}
