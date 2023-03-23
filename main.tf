d84cc5718ffe86a872afe4d8add8728e6a492974
Srini K <srinikdevops0131@gmail.com>
2023-03-23 06:18:30 -0700

    updating ./tf-module-rds

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_rds_cluster" "main" {
   preferred_backup_window = var.preferred_backup_window
   db_subnet_group_name    = aws_db_subnet_group.main.name
   #kms_key_id              = data.aws_kms_key.key.arn
-  storage_encrypted       = var.storage_encrypted
+  skip_final_snapshot       = true
 
   tags = merge(
     var.tags,
