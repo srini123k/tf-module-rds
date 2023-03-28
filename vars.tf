c94556bde6d192f7b9ed19cba063f155c9dd7d18
Srini K <srinikdevops0131@gmail.com>
2023-03-28 14:52:48 -0700

    updating ./tf-module-rds

--- vars.tf
+++ vars.tf
@@ -10,3 +10,6 @@ variable "backup_retention_period" {}
 variable "preferred_backup_window" {}
 variable "no_of_instances" {}
 variable "instance_class" {}
+
+#variable "vpc_id" {}
+#variable "allow_subnets" {}
