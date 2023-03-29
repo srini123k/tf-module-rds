28a79a9d1b377970f05c54f823288b36b89cf1bf
Srini K <srinikdevops0131@gmail.com>
2023-03-28 22:23:37 -0700

    updating ./tf-module-rds

--- main.tf
+++ main.tf
@@ -10,40 +10,40 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name    = aws_db_subnet_group.main.name
   #kms_key_id              = data.aws_kms_key.key.arn
   skip_final_snapshot       = true
-#vpc_security_group_ids  = [aws_security_group.main.id]
+  vpc_security_group_ids  = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
     { Name = "${var.env}-rds" }
   )
 }
-#
-#resource "aws_security_group" "main" {
-#  name        = "rds-${var.env}"
-#  description = "rds-${var.env}"
-#  vpc_id      = var.vpc_id
-#
-#  ingress {
-#    description = "RDS"
-#    from_port   = 3306
-#    to_port     = 3306
-#    protocol    = "tcp"
-#    cidr_blocks = var.allow_subnets
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "rds-${var.env}" }
-#  )
-#}
+
+resource "aws_security_group" "main" {
+  name        = "rds-${var.env}"
+  description = "rds-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "RDS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "rds-${var.env}" }
+  )
+}
 
 resource "aws_rds_cluster_instance" "main" {
   count              = var.no_of_instances
@@ -63,10 +63,10 @@ resource "aws_db_subnet_group" "main" {
     { Name = "${var.env}-subnet-group" }
   )
 }
-#
-#resource "aws_ssm_parameter" "rds_endpoint" {
-#  name  = "${var.env}.rds.endpoint"
-#  type  = "String"
-#  value = aws_rds_cluster.main.endpoint
-#}
+
+resource "aws_ssm_parameter" "rds_endpoint" {
+  name  = "${var.env}.rds.endpoint"
+  type  = "String"
+  value = aws_rds_cluster.main.endpoint
+}
 
--- vars.tf
+++ vars.tf
@@ -11,5 +11,5 @@ variable "preferred_backup_window" {}
 variable "no_of_instances" {}
 variable "instance_class" {}
 
-#variable "vpc_id" {}
-#variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
